cbcf38727c45e5c84364fd9905c6f30f249b68a5
Edwin Meek <emeekiv@gmail.com>
2019-12-20 13:46:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // add solution here
-/*function theBeatlesPlay(musicians, instruments){
+function theBeatlesPlay(musicians, instruments){
 var list = [];
 var mlen = musicians.length;
   for (let i = 1; i <= mlen; i++){
     list.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return list;
-} */
+}
 
 /*function theBeatlesPlay(musicians, instruments){
 var list = [];

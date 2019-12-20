98eba9e7f75433e8ff5cdc7bcf2b759fdb74f613
Edwin Meek <emeekiv@gmail.com>
2019-12-20 12:55:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+var list = []
+var mlen = musicians.length
+  for (var i = 1; i <= mlen; i++){
+    list.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return list
+}

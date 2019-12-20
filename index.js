49c98023a71996d59c8dfb5b78b33a74ed7e0f18
Edwin Meek <emeekiv@gmail.com>
2019-12-20 13:18:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 // add solution here
-function theBeatlesPlay(musicians, instruments){
+/* function theBeatlesPlay(musicians, instruments){
 var list = [];
 var mlen = musicians.length;
   for (let i = 1; i <= mlen; i++){
     list.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return list;
+} */
+
+function theBeatlesPlay(musicians, instruments){
+var list = [];
+var mlen = musicians.length;
+  for (let i = 1; i <= mlen; i++){
+      var musician = musicians[i];
+      var instrument = instrument[i];
+    list.push(`${musician} plays ${instrument}`);
   }
   return list;
 }
